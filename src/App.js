304ee402266ff83b667667170f5d3b76fe922673
Ruslan Fedko <diamondmr84@gmail.com>
2023-09-08 22:18:48 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,99 @@
+import './App.css';
+import MmLogo from './static/images/MM-LOGO.png';
+import {HomeOutlined, IssuesCloseOutlined, LogoutOutlined, ScheduleOutlined, SettingOutlined} from "@ant-design/icons";
+import {Input} from "antd";
+import {useState} from "react";
+import classNames from "classnames";
+
+const {Search} = Input;
+
+function App() {
+    const [selectedItem, setSelectedItem] = useState('home');
+
+    return (
+        <div className="App__Container">
+            <div className="Menu__Container">
+                <div className="Menu__Logo">
+                    <div className="Menu__Logo-IMG">
+                        <img src={MmLogo} alt=""/>
+                    </div>
+                    <div className="Menu__Logo-Title">
+                        Миранда-Медиа
+                    </div>
+                </div>
+                <div className="Menu__Items">
+                    <div className={
+                             classNames(
+                                 'Menu__Item',
+                                 {'Menu__Item--Active' : selectedItem === 'home'}
+                             )}
+                         onClick={()=>setSelectedItem('home')}>
+                        <HomeOutlined className={
+                            classNames(
+                                'Menu__Item-Logo',
+                                {'Menu__Item-Logo--Active' : selectedItem === 'home'}
+                            )} />
+                        <span className={classNames(
+                            'Menu__Item-Text',
+                            {'Menu__Item-Text--Active' : selectedItem === 'home'}
+                        )}>Главная</span>
+                    </div>
+                    <div className={
+                        classNames(
+                            'Menu__Item',
+                            {'Menu__Item--Active' : selectedItem === 'inventory'}
+                        )} onClick={()=>setSelectedItem('inventory')}>
+                        <ScheduleOutlined className={
+                            classNames(
+                                'Menu__Item-Logo',
+                                {'Menu__Item-Logo--Active' : selectedItem === 'inventory'}
+                            )} />
+                        <span className={classNames(
+                            'Menu__Item-Text',
+                            {'Menu__Item-Text--Active' : selectedItem === 'inventory'}
+                        )}>Включения</span>
+                    </div>
+                    <div className={
+                        classNames(
+                            'Menu__Item',
+                            {'Menu__Item--Active' : selectedItem === 'tickets'}
+                        )} onClick={()=>setSelectedItem('tickets')}>
+                        <IssuesCloseOutlined className={
+                            classNames(
+                                'Menu__Item-Logo',
+                                {'Menu__Item-Logo--Active' : selectedItem === 'tickets'}
+                            )} />
+                        <span className={classNames(
+                            'Menu__Item-Text',
+                            {'Menu__Item-Text--Active' : selectedItem === 'tickets'}
+                        )}>Тикеты</span>
+                    </div>
+                </div>
+                <div className="Menu__Bottom">
+                    <div className="Menu__Settings">
+                        <SettingOutlined  className='Menu__Settings-Logo'/>
+                        <span className='Menu__Settings-Text'>Настройки</span>
+                    </div>
+                    <div className="Menu__Logout">
+                        <LogoutOutlined  className='Menu__Logout-Logo'/>
+                        <span className='Menu__Logout-Text'>Выйти</span>
+                    </div>
+                </div>
+            </div>
+            <div className="Layout__Container">
+                <div className="Header__Container">
+                    <div className="Header__Search">
+                        <Search placeholder="Начните что-то искать"
+                                rootClassName='Header__Search-Input'
+                                allowClear size={'large'} />
+                    </div>
+                    <div className="Header__User">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;

9a1727c0e9835e4022ccac3d75c2a154744771f9
Ruslan Fedko <diamondmr84@gmail.com>
2023-09-11 11:59:21 +0300

    Add Fake Data

--- src/Pages/Channels/Channels.js
+++ src/Pages/Channels/Channels.js
@@ -3,261 +3,48 @@ import _InnerPage from "../_InnerPage/_InnerPage";
 import { DataTable } from 'primereact/datatable';
 import {Column} from "primereact/column";
 import './Channels.css';
+import fakeData from '../../static/staticData/fake_data.json';
+import {Tag} from "primereact/tag";
 
-const data = [
-    {
-        _id: 'asdjkah2',
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },{
-        id: 'СУЗ-12312',
-        id_oss: "asda",
-        client: "РТПЦ",
-        service: "L2VPN",
-        city: "Новофёдоровка",
-        street: "Парковое ш.",
-        home: "39",
-        status: "ВКЛ",
-        date: "04.08.2023"
-    },
-]
 function Channels(props) {
 
     const [selectedChannel, setSelectedChannel] = useState(null);
 
+    const getStatusColor = (status)=>{
+        switch (status) {
+            case 'ВКЛ':
+                return 'success';
+            case 'ВЫКЛ':
+                return 'danger';
+            case 'ИЗМ':
+                return 'info';
+        }
+    }
+
+    const statusBodyTemplate = (rowData) => {
+        return <Tag style={{width: "50px",}} value={rowData.status} severity={getStatusColor(rowData.status)}></Tag>;
+    };
+
     return (
        <_InnerPage>
            <div className="ChannelsPage__Container">
-               <DataTable value={data} paginator rows={13} className={'ChannelsPage__Table-Container'}
+               <div className="ChannelsPage__Filters">
+                Фильтры
+               </div>
+               <DataTable value={fakeData} paginator rows={10} className={'ChannelsPage__Table-Container'}
                           selectionMode="single" selection={selectedChannel} onSelectionChange={(e) => setSelectedChannel(e.value)} dataKey="_id"
                           metaKeySelection={false}>
-                   <Column field="id" header="ID"></Column>
+                   <Column field="id" header="ID" style={{maxWidth: '60px', overflow: 'clip'}}></Column>
                    <Column field="id_oss" header="ID_OSS"></Column>
                    <Column field="client" header="Клиент"></Column>
                    <Column field="service" header="Услуга"></Column>
                    <Column field="city" header="Нас.Пункт"></Column>
                    <Column field="street" header="Улица"></Column>
                    <Column field="home" header="Дом"></Column>
-                   <Column field="status" header="Статус"></Column>
+                   <Column field="status" header="Статус" body={statusBodyTemplate}></Column>
                    <Column field="date" header="Дата"></Column>
                </DataTable>
+
            </div>
        </_InnerPage>
     );

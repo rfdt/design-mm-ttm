f755c92a25f3bee2925c3cf1d08a66b345378e42
Ruslan Fedko <diamondmr84@gmail.com>
2023-09-12 22:53:46 +0300

    Fix Some Table View

--- src/Pages/Channels/ChannelsTable/ChannelsTable.js
+++ src/Pages/Channels/ChannelsTable/ChannelsTable.js
@@ -4,8 +4,12 @@ import {Column} from "primereact/column";
 import {DataTable} from "primereact/datatable";
 import {Tag} from "primereact/tag";
 import {ContextMenu} from "primereact/contextmenu";
+import './ChannelsTable.css';
 
 function ChannelsTable({setSelectedChannel, selectedChannel}) {
+
+    //TODO Добить идею с таблицей, подумать нужно ли добавлять элементы, или пусть таблица по высоте сжимается,
+    //поразмыслить и что-то в голову придёт
 
     const [contextSelected, setContextSelected] = useState(null);
 
@@ -14,6 +18,8 @@ function ChannelsTable({setSelectedChannel, selectedChannel}) {
         {label: 'Просмотреть', icon: 'pi pi-fw pi-eye', command: ()=> setSelectedChannel(contextSelected)},
         {label: 'Копировать СУЗ', icon: 'pi pi-fw pi-copy', command: ()=>console.log(contextSelected)} //Функция на копирования СУЗа в бу.обмен
     ];
+
+    const [sortedData, setSortedData] = useState(fakeData.slice(0,12));
 
     const getStatusColor = (status) => {
         switch (status) {
@@ -30,6 +36,11 @@ function ChannelsTable({setSelectedChannel, selectedChannel}) {
         }
     }
 
+    const isRowSelectable = (e) =>{
+        if(Object.keys(e.data).includes('fake')) return false;
+        return true;
+    }
+
     const statusBodyTemplate = (rowData) => {
         return <Tag style={{width: "50px",}} value={rowData.status} severity={getStatusColor(rowData.status)}></Tag>;
     };
@@ -37,24 +48,24 @@ function ChannelsTable({setSelectedChannel, selectedChannel}) {
     return (
         <>
             <ContextMenu model={menuModel} ref={cm} onHide={() => setContextSelected(null)}/>
-            <DataTable value={fakeData} paginator rows={10} className={'ChannelsPage__Table-Container'}
+            <DataTable value={sortedData} scrollable scrollHeight={'700px'} className={'ChannelsPage__Table-Container'}
                        selectionMode="single" selection={selectedChannel}
                        onSelectionChange={(e) => setSelectedChannel(e.value)} dataKey="_id"
                        onContextMenu={(e) => cm.current.show(e.originalEvent)}
                        contextMenuSelection={contextSelected}
                        onContextMenuSelectionChange={(e) => setContextSelected(e.value)}
-                       metaKeySelection={false}
+                       metaKeySelection={false} /*isDataSelectable={isRowSelectable}*/
             >
-                <Column field="id" header="ID" style={{width: '55px', overflow: 'clip'}}></Column>
-                <Column field="id_oss" header="ID_OSS"></Column>
-                <Column field="client" header="Клиент"></Column>
-                <Column field="service" header="Услуга"></Column>
-                <Column field="size" header="Объём"></Column>
-                <Column field="city" header="Нас.Пункт"></Column>
-                <Column field="street" header="Улица"></Column>
-                <Column field="home" header="Дом"></Column>
-                <Column field="status" header="Статус" body={statusBodyTemplate}></Column>
-                <Column field="date" header="Дата"></Column>
+                <Column field="id" header="ID" className='ChannelsPage__Table-ID-Field'></Column>
+                <Column field="id_oss" header="ID_OSS" className='ChannelsPage__Table-ID-OSS-Field'></Column>
+                <Column field="client" header="Клиент" className='ChannelsPage__Table-Client-Field'></Column>
+                <Column field="service" header="Услуга" className='ChannelsPage__Table-Service-Field'></Column>
+                <Column field="size" header="Объём" className='ChannelsPage__Table-Size-Field'></Column>
+                <Column field="city" header="Нас.Пункт" className='ChannelsPage__Table-City-Field'></Column>
+                <Column field="street" header="Улица" className='ChannelsPage__Table-Street-Field'></Column>
+                <Column field="home" header="Дом" className='ChannelsPage__Table-Home-Field'></Column>
+                <Column field="status" header="Статус" body={statusBodyTemplate} className='ChannelsPage__Table-Status-Field'></Column>
+                <Column field="date" header="Дата" className='ChannelsPage__Table-Date-Field'></Column>
             </DataTable>
         </>
     );

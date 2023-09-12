389447e7ee7c9a7fa3008c527fab8c0c2c0bb36b
Ruslan Fedko <diamondmr84@gmail.com>
2023-09-12 15:42:10 +0300

    Filters

--- src/Pages/Channels/СhannelsFilters/ChannelsFilters.js
+++ src/Pages/Channels/СhannelsFilters/ChannelsFilters.js
@@ -3,6 +3,7 @@ import './ChannelsFilters.css';
 import {InputText} from "primereact/inputtext";
 import {Button} from "primereact/button"
 import {AutoComplete} from "primereact/autocomplete";
+import {Dropdown} from "primereact/dropdown";
 
 const ITEMS = [
     "Симферополь г.",
@@ -46,7 +47,7 @@ function ChannelsFilters(props) {
             <div className="ChannelsFilters__Base">
                 <div className="ChannelsFilters__AdditionalSearch">
                     <InputText type="text"
-                               className=" ChannelsFilters__AdditionalSearch-Input"
+                               className="ChannelsFilters__AdditionalSearch-Input"
                                placeholder="ID / Клиент / Доп.Инфо / Примечание" />
                     <Button icon="pi pi-search"
                             className={'ChannelsFilters__AdditionalSearch-SearchBtn'}  rounded severity="info"/>
@@ -56,16 +57,36 @@ function ChannelsFilters(props) {
                 <div className="ChannelsFilters__MainSearch">
                     <AutoComplete   value={cityValue} suggestions={filteredCitySuggestions} completeMethod={cityCompleteMethod}
                                   onChange={(e) => setCityValue(e.value)} dropdown
-                                    placeholder='Населенный пункт'
+                                    placeholder='Населенный пункт' className={'ChannelsFilters__MainSearch-City'}
                     />
                     <AutoComplete  value={streetsValue} suggestions={filteredStreetsSuggestions} completeMethod={streetsCompleteMethod}
                                   onChange={(e) => setStreetsValue(e.value)} dropdown
-                                  placeholder='Улица'
+                                  placeholder='Улица' className={'ChannelsFilters__MainSearch-Street'}
+                    />
+                    <InputText type="text"
+                               className={'ChannelsFilters__MainSearch-Home'}
+                               placeholder='Дом'
+                    />
+                    <Dropdown options={[]}
+                              optionLabel="name"
+                              placeholder="Услуга" className="ChannelsFilters__MainSearch-Service"
+                    />
+                    <Dropdown options={[]}
+                              optionLabel="name"
+                              placeholder="Статус" className={'ChannelsFilters__MainSearch-Status'}
                     />
                 </div>
             </div>
             <div className="ChannelsFilters__Info">
-
+                <div className="ChannelsFilters__ExtendedSearch-Info">
+                    Найдено: 123
+                </div>
+                <div className="ChannelsFilters__ExtendedSearch-Btns">
+                    <Button icon="pi pi-sliders-h" severity="info" className='ChannelsFilters__ExtendedSearch-Btn'/>
+                    <Button icon="pi pi-file-excel" severity="success" className='ChannelsFilters__ExtendedSearch-Btn'/>
+                    <Button icon="pi pi-plus-circle" disabled onClick={()=>console.log(123)} className='ChannelsFilters__ExtendedSearch-Btn'/>
+                    <Button icon="pi pi-database" disabled severity="help" onClick={()=>console.log(123)} className='ChannelsFilters__ExtendedSearch-Btn'/>
+                </div>
             </div>
         </div>
     );
